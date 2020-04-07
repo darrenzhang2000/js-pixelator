76ed4cc9066eb097d6b1774af0ceebaa7192a129
Darren Zhang <darrenzhang200027@gmail.com>
2020-04-07 03:58:08 -0400

    added addRow

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const table = Document.getElementsByTagName('table')
+const table = document.getElementsByTagName('table')[0]
+var numRows = 3
+var numCols = 3
+
 const addRow = () => {
-    
+    const row = document.createElement('tr')
+    for(i = 0; i < numCols; i++){
+        console.log('td created')
+        const col = document.createElement('td')
+        const text = document.createTextNode('Pika')
+        col.appendChild(text)
+        row.appendChild(col)
+    }
+    table.appendChild(row)
 }
+
+
